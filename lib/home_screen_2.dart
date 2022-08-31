daa566ece4aa72612570bc422995ad76c16b330c
Mohit Dev <mohit.solankee@bacancy.com>
2022-08-31 17:47:44 +0530

    testing home screen added

---
+++ lib/home_screen_2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen2 extends StatefulWidget {
+  const HomeScreen2({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen2> createState() => _HomeScreen2State();
+}
+
+class _HomeScreen2State extends State<HomeScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
